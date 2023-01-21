f828026a41cd86fe8058274124f1b38123211296
1shan-pixel <ispanta03@gmail.com>
2023-01-21 15:41:29 +0545

    hero image added

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer";
+import HeroImg from "../components/HeroImg";
 import Slider from "../components/Slider";
 
 function Home() {
   return (
     <>
+      <HeroImg/>
       <Slider />
+      <Footer/>
     </>
   );
 }
