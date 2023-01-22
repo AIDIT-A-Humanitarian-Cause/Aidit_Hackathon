c6b0cf19a73904782186a224aab2280fbcf75c3d
1shan-pixel <ispanta03@gmail.com>
2023-01-22 13:36:22 +0545

    about and donate done

--- client/src/home/Needy.js
+++ client/src/home/Needy.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import { ItemsToBeInSlide } from "./Datas";
+import { ItemsToBeInSlide } from "./datas";
 import { useState } from "react";
 
 const Container = styled.div`
